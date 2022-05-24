28ababf9377806f1b9cfab7e27667495b757478f
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2022-05-24 17:23:47 +0300

    Added out postfix option

--- constants.js
+++ constants.js
@@ -2,11 +2,18 @@ const Types = require('./lib/types');
 
 const path = require('path');
 const config = require(path.join(process.cwd(), 'package.json'));
+const defaults = require('./default.json');
+const { setDefaultMinFiles } = require('./lib/tools/utils');
 
 /**
  * @type {Types.CatalogReducerConfig}
  */
-const catalogReducer = config.catalogReducer || require('./default.json');
+const catalogReducer = config.catalogReducer || defaults;
+const src = catalogReducer.src;
+
+const defaultMinEnding = catalogReducer.outPostfix + '.xml';
+
+setDefaultMinFiles(src, defaultMinEnding);
 
 module.exports.catalogReducer = catalogReducer;
 module.exports.enabledCache = catalogReducer.enabledCache;
