0043e7997b66b46e8f7001ad871900915f1e6b8e
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2022-05-24 16:10:26 +0300

    Added types for CatalogReducerConfig

--- constants.js
+++ constants.js
@@ -1,6 +1,11 @@
+const Types = require('./lib/types');
+
 const path = require('path');
 const config = require(path.join(process.cwd(), 'package.json'));
 
+/**
+ * @type {Types.CatalogReducerConfig}
+ */
 const catalogReducer = config.catalogReducer || require('./default.json');
 
 module.exports.catalogReducer = catalogReducer;
