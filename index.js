a6d8d5a9064aea97789d0d87010207d63e97ab95
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2022-05-24 16:49:17 +0300

    Added generation of inventory catalog

--- index.js
+++ index.js
@@ -24,11 +24,56 @@ const categoriesWorker = new NavigationCategoriesWorker(src.navigation);
 const assignmentsWorker = new NavigationAssignmentsWorker(src.navigation);
 const masterWorker = new MasterCatalogWorker(src.master);
 
-const filterProductByNavigationRegistry = (/** @type {Types.XMLTag} */ tag) => {
+const filterProductByOptimizedRegistry = (/** @type {Types.XMLTag} */ tag) => {
     const id = tag.attributes['product-id'];
     const { finalProductList } = assignmentsWorker.registry.cache;
 
     return id in finalProductList;
+}
+
+/**
+ * @param {NavigationAssignmentsWorker} assignmentsWorker
+ * @param {NavigationCategoriesWorker} categoriesWorker
+ */
+const generanaMinifiedNavigatoin = async (assignmentsWorker, categoriesWorker) => {
+    const tempMinifiedNavigationWithFilteredProducts = src.minifiedNavigation + '.temp';
+
+    const navigationFilterByProducts = new XMLFilterWriter(src.navigation, tempMinifiedNavigationWithFilteredProducts);
+
+    await navigationFilterByProducts.startAsync('category-assignment', filterProductByOptimizedRegistry);
+
+    const navigationFilterByCategories = new XMLFilterWriter(tempMinifiedNavigationWithFilteredProducts, src.minifiedNavigation);
+
+    const onlyUsedCategories = assignmentsWorker.registry.getFinalUsedCategories();
+
+    const usedCategoriesWithParents = categoriesWorker.registry.filterCategories(onlyUsedCategories);
+
+    await navigationFilterByCategories.startAsync('category', tag => {
+        const categoryId = tag.attributes['category-id'];
+
+        return usedCategoriesWithParents.includes(categoryId);
+    });
+}
+
+const generateMinifiedMaster = async () => {
+    const tempMinifiedMasterWithFilteredProducts = src.minifiedMaster + '.temp';
+
+    const masterFilterByProduct = new XMLFilterWriter(src.master, tempMinifiedMasterWithFilteredProducts);
+
+    /**
+     * Filter product by final registry in navigation catalog
+     */
+    await masterFilterByProduct.startAsync('product', filterProductByOptimizedRegistry);
+
+    const masterFilterByAssignments = new XMLFilterWriter(tempMinifiedMasterWithFilteredProducts, src.minifiedMaster);
+
+    await masterFilterByAssignments.startAsync('category-assignment', filterProductByOptimizedRegistry);
+}
+
+const generateMinifiedInventory = async () => {
+    const inventoryFilterByProduct = new XMLFilterWriter(src.inventory, src.minifiedInventory);
+
+    await inventoryFilterByProduct.startAsync('record', filterProductByOptimizedRegistry);
 }
 
 /**
@@ -62,36 +107,11 @@ const filterProductByNavigationRegistry = (/** @type {Types.XMLTag} */ tag) => {
      */
     assignmentsWorker.registry.optimize(categoriesConfig, productsConfig);
 
-    const tempMinifiedMasterWithFilteredProducts = src.minifiedMaster + '.temp';
+    await generateMinifiedMaster();
 
-    const masterFilterByProduct = new XMLFilterWriter(src.master, tempMinifiedMasterWithFilteredProducts);
+    await generanaMinifiedNavigatoin(assignmentsWorker, categoriesWorker);
 
-    /**
-     * Filter product by final registry in navigation catalog
-     */
-    await masterFilterByProduct.startAsync('product', filterProductByNavigationRegistry);
-
-    const masterFilterByAssignments = new XMLFilterWriter(tempMinifiedMasterWithFilteredProducts, src.minifiedMaster);
-
-    masterFilterByAssignments.startAsync('category-assignment', filterProductByNavigationRegistry);
-
-    const tempMinifiedNavigationWithFilteredProducts = src.minifiedNavigation + '.temp';
-
-    const navigationFilterByProducts = new XMLFilterWriter(src.navigation, tempMinifiedNavigationWithFilteredProducts);
-
-    await navigationFilterByProducts.startAsync('category-assignment', filterProductByNavigationRegistry);
-
-    const navigationFilterByCategories = new XMLFilterWriter(tempMinifiedNavigationWithFilteredProducts, src.minifiedNavigation);
-
-    const onlyUsedCategories = assignmentsWorker.registry.getFinalUsedCategories();
-
-    const usedCategoriesWithParents = categoriesWorker.registry.filterCategories(onlyUsedCategories);
-
-    await navigationFilterByCategories.startAsync('category', tag => {
-        const categoryId = tag.attributes['category-id'];
-
-        return usedCategoriesWithParents.includes(categoryId);
-    });
+    await generateMinifiedInventory();
 
     if (catalogReducer.cleanupData) {
         cleanupFolders();
