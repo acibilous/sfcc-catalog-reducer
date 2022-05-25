f82bb3ed521ce947353bdb9e4f346ec5e1a486f0
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2022-05-25 15:45:32 +0300

    Divided reducers into separate files, discarded usage of 'out file src' option

--- constants.js
+++ constants.js
@@ -3,18 +3,11 @@ const Types = require('./lib/types');
 const path = require('path');
 const config = require(path.join(process.cwd(), 'package.json'));
 const defaults = require('./default.json');
-const { setDefaultMinFiles } = require('./lib/tools/files');
 
 /**
  * @type {Types.CatalogReducerConfig}
  */
 const catalogReducer = config.catalogReducer || defaults;
-const src = catalogReducer.src;
-
-const defaultMinEnding = catalogReducer.outPostfix + '.xml';
-
-setDefaultMinFiles(src, defaultMinEnding);
 
 module.exports.catalogReducer = catalogReducer;
 module.exports.enabledCache = catalogReducer.enabledCache;
-module.exports.defaultMinEnding = defaultMinEnding;
--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const NavigationCategoriesWorker = require('./lib/catalog/workers/NavigationCategoriesWorker');
 const NavigationAssignmentsWorker = require('./lib/catalog/workers/NavigationAssignmentsWorker');
 const MasterCatalogWorker = require('./lib/catalog/workers/MasterCatalogWorker');
-const XMLFilterWriter = require('./lib/xml/XMLFilterWriter');
-const { getCleaner } = require('./lib/tools/cleanup');
-const { getXMLFilesList, getMinifieldFile } = require('./lib/tools/files');
 
-const { catalogReducer, defaultMinEnding } = require('./constants');
+const { getCleaner } = require('./lib/tools/cleanup');
+
+const { catalogReducer } = require('./constants');
+const getReducers = require('./lib/tools/reducers');
 
 const {
     productsConfig,
@@ -24,70 +24,6 @@ if (!catalogReducer.enabledCache) {
 const categoriesWorker = new NavigationCategoriesWorker(src.navigation);
 const assignmentsWorker = new NavigationAssignmentsWorker(src.navigation);
 const masterWorker = new MasterCatalogWorker(src.master);
-
-const filterProductByOptimizedRegistry = (/** @type {Types.XMLTag} */ tag) => {
-    const id = tag.attributes['product-id'];
-    const { finalProductList } = assignmentsWorker.registry.cache;
-
-    return id in finalProductList;
-}
-
-/**
- * @param {NavigationAssignmentsWorker} assignmentsWorker
- * @param {NavigationCategoriesWorker} categoriesWorker
- */
-const generanaMinifiedNavigatoin = async (assignmentsWorker, categoriesWorker) => {
-    const tempMinifiedNavigationWithFilteredProducts = src.minifiedNavigation + '.temp';
-
-    const navigationFilterByProducts = new XMLFilterWriter(src.navigation, tempMinifiedNavigationWithFilteredProducts);
-
-    await navigationFilterByProducts.startAsync('category-assignment', filterProductByOptimizedRegistry);
-
-    const navigationFilterByCategories = new XMLFilterWriter(tempMinifiedNavigationWithFilteredProducts, src.minifiedNavigation);
-
-    const onlyUsedCategories = assignmentsWorker.registry.getFinalUsedCategories();
-
-    const usedCategoriesWithParents = categoriesWorker.registry.filterCategories(onlyUsedCategories);
-
-    await navigationFilterByCategories.startAsync('category', tag => {
-        const categoryId = tag.attributes['category-id'];
-
-        return usedCategoriesWithParents.includes(categoryId);
-    });
-}
-
-const generateMinifiedMaster = async () => {
-    const tempMinifiedMasterWithFilteredProducts = src.minifiedMaster + '.temp';
-
-    const masterFilterByProduct = new XMLFilterWriter(src.master, tempMinifiedMasterWithFilteredProducts);
-
-    /**
-     * Filter product by final registry in navigation catalog
-     */
-    await masterFilterByProduct.startAsync('product', filterProductByOptimizedRegistry);
-
-    const masterFilterByAssignments = new XMLFilterWriter(tempMinifiedMasterWithFilteredProducts, src.minifiedMaster);
-
-    await masterFilterByAssignments.startAsync('category-assignment', filterProductByOptimizedRegistry);
-}
-
-const generateMinifiedInventory = async () => {
-    const inventoryFilterByProduct = new XMLFilterWriter(src.inventory, src.minifiedInventory);
-
-    await inventoryFilterByProduct.startAsync('record', filterProductByOptimizedRegistry);
-}
-
-const generatePriceBooks = async () => {
-    const priceBooks = await getXMLFilesList(src.pricebooksDir, f => !f.includes(defaultMinEnding));
-
-    return Promise.all(priceBooks.map((file) => {
-        const minFile = getMinifieldFile(file, defaultMinEnding);
-
-        const priceFilterByProduct = new XMLFilterWriter(file, minFile);
-
-        return priceFilterByProduct.startAsync('price-table', filterProductByOptimizedRegistry);
-    }));
-}
 
 /**
  * @description Entry point
@@ -120,13 +56,15 @@ const generatePriceBooks = async () => {
      */
     assignmentsWorker.registry.optimize(categoriesConfig, productsConfig);
 
-    await generateMinifiedMaster();
+    const reducers = getReducers(assignmentsWorker.registry.cache.finalProductList, src);
 
-    await generanaMinifiedNavigatoin(assignmentsWorker, categoriesWorker);
+    await reducers.master();
 
-    await generateMinifiedInventory();
+    await reducers.navigation(assignmentsWorker, categoriesWorker);
 
-    await generatePriceBooks();
+    await reducers.inventory();
+
+    await reducers.priceBook();
 
     if (catalogReducer.cleanupData) {
         cleanupFolders();
