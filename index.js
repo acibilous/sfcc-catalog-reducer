99d3a07b8ba09793cd8899ade55e531ef774466b
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2022-05-30 17:52:19 +0300

    Added support of multiple navigation catalogs

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import NavigationCategoriesWorker from './lib/catalog/workers/NavigationCategori
 import NavigationAssignmentsWorker from './lib/catalog/workers/NavigationAssignmentsWorker.js';
 import MasterCatalogWorker from './lib/catalog/workers/MasterCatalogWorker.js';
 
+import NavigationAssignmentsRegistry from './lib/catalog/registry/NavigationAssignmentsRegistry.js';
+import NavigationCategoriesRegistry from './lib/catalog/registry/NavigationCategoriesRegistry.js';
+
 import { getCleaner, renameReducedToOriginal } from './lib/tools/cleanup.js';
+import { getFilesByPatterns } from './lib/tools/files.js';
 
 import { catalogReducer } from './constants.js';
 import getReducers from './lib/tools/reducers.js';
@@ -15,8 +19,6 @@ const {
     src
 } = catalogReducer;
 
-const categoriesWorker = new NavigationCategoriesWorker(src.navigation);
-const assignmentsWorker = new NavigationAssignmentsWorker(src.navigation);
 const masterWorker = new MasterCatalogWorker(src.master);
 
 /**
@@ -31,20 +33,32 @@ const masterWorker = new MasterCatalogWorker(src.master);
         cleanupFolders();
     }
 
+    const categoriesWorkers = (await getFilesByPatterns(src.navigations)).map(file => new NavigationCategoriesWorker(file));
+    const assignmentsWorkers = (await getFilesByPatterns(src.navigations)).map(file => new NavigationAssignmentsWorker(file));
+
     /**
      * Parsing all required data
      */
     await Promise.all([
         masterWorker.startAsync(),
-        assignmentsWorker.startAsync(),
-        categoriesWorker.startAsync()
+        assignmentsWorkers.map(worker => worker.startAsync()),
+        categoriesWorkers.map(worker => worker.startAsync())
     ]);
+
+    const singleCategoryRegistry = new NavigationCategoriesRegistry(process.cwd());
+    const singleAssignmentRegistry = new NavigationAssignmentsRegistry(process.cwd());
+
+    singleAssignmentRegistry.appendCategories(assignmentsWorkers.map(worker => worker.registry.cache.categories));
+    singleCategoryRegistry.appendCategoriesParrents(categoriesWorkers.map(worker => worker.registry.cache.categoriesParents));
+
 
     /**
      * Adding dependencies to navigation catalog registry. Product may be not category assignment
      * but his owner assigned to navigation catalog
      */
-    assignmentsWorker.registry.updateProducts(masterWorker.registry.cache.products);
+    singleAssignmentRegistry.updateProducts(masterWorker.registry.cache.products);
+
+
 
     /**
      * We have all needed data, so we don't need master catalog registry
@@ -54,13 +68,13 @@ const masterWorker = new MasterCatalogWorker(src.master);
     /**
      * Now we may reduce catalog data by configuration
      */
-    assignmentsWorker.registry.optimize(categoriesConfig, productsConfig);
+    singleAssignmentRegistry.optimize(categoriesConfig, productsConfig);
 
-    const reducers = getReducers(assignmentsWorker.registry.cache.finalProductList, src);
+    const reducers = getReducers(singleAssignmentRegistry.cache.finalProductList, src);
 
     await Promise.all([
         reducers.master(),
-        reducers.navigation(assignmentsWorker, categoriesWorker),
+        reducers.navigation(singleAssignmentRegistry.getFinalUsedCategories(), singleCategoryRegistry),
         reducers.inventory(),
         reducers.priceBook()
     ]);
--- lib/types.ts
+++ lib/types.ts
@@ -29,7 +29,7 @@ export type CategoriesConfig = {
 
 export type SrcConfig = {
     master: string;
-    navigation: string;
+    navigations: Array<string>;
     inventories: Array<string>;
     pricebooks: Array<string>;
 };
